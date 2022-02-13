f98dab17d8ba94250f4179989375ba28fe030023
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-02-14 00:34:00 +0600

    generate random pin added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('It working');
+function getPin() {
+    const pin = Math.round(Math.random() * 1000000);
+    const pinString = pin + '';
+    console.log(pinString.length)
+    if (pinString.length == 6) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('random-pin').value = pin;
+}
